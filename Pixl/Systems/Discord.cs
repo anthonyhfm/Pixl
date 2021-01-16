d37345c8e57a8a07b202d0752f0929bbb873e643
Anthony Hofmeister <76816347+anthonyhfm@users.noreply.github.com>
2021-01-16 14:07:18 +0100

    Discord RPC Update

--- Pixl/Systems/Discord.cs
+++ Pixl/Systems/Discord.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DiscordRPC;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,9 +8,39 @@ namespace Pixl.Systems
     public class Discord
     {
 
-        public void ActivateRPC()
-        {
+        public DiscordRpcClient client;
 
+		public class DiscordDetails
+        {
+			public string ClientID { get; set; }
+
+			public string Details { get; set; }
+
+			public string State { get; set; }
+
+			public Assets Assets { get; set; }
+        }
+
+        public void Initialize(DiscordDetails details)
+        {
+			client = new DiscordRpcClient(details.ClientID);
+
+			//Connect to the RPC
+			client.Initialize();
+
+			//Set the rich presence
+			//Call this as many times as you want and anywhere in your code.
+			client.SetPresence(new RichPresence()
+			{
+				Details = details.Details,
+				State = details.State,
+				Assets = details.Assets
+			});
+		}
+
+		public void Update()
+        {
+			client.Invoke();
         }
 
     }
